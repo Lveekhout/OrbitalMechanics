ff19effc879da1487549d0960211e0c2b4753fec
laurensvaneekhout <eekhout.l@tkppensioen.nl>
2025-11-01 13:43:36 +0100

    Tussentijds bijschrijven

--- circular-motions/js/index.js
+++ circular-motions/js/index.js
@@ -2,69 +2,24 @@ let r
 let theta
 let rx, ry, vx, vy, ax, ay, jx, jy
 
-const camera = {x: 460, y: 320, scale: 100}
-const visor = { x: null, y: null, visible: false }
-
-const anim = new Animation()
+const anim = new Animation2d()
 
 window.onload = e => {
-    anim.set.add(draw)
+    anim.registerCanvas(document.getElementById('canvas'), {x: 460, y: 320, scale: 100}, draw)
 
     document.querySelector('input#input1').dispatchEvent(new Event('input'))
     document.querySelector('input#input2').dispatchEvent(new Event('input'))
-
-    document.getElementById('canvas').addEventListener('mousemove', event => {
-        window.requestAnimationFrame(draw)
-        event.preventDefault()
-
-        visor.x = event.offsetX
-        visor.y = event.offsetY
-
-        if (event.ctrlKey) {
-            camera.x = event.offsetX
-            camera.y = event.offsetY
-        } else if (event.buttons === 1) {
-            camera.x += event.movementX
-            camera.y += event.movementY
-        }
-    })
-
-    document.getElementById('canvas').addEventListener('mousewheel', event => {
-        window.requestAnimationFrame(draw)
-        event.preventDefault()
-
-        const scaleBefore = camera.scale
-
-        camera.scale *= 1 - event.deltaY / 1000
-        camera.scale = Math.min(Math.max(camera.scale, 10), 10000)
-
-        camera.x = event.offsetX - ((event.offsetX - camera.x) / scaleBefore) * camera.scale
-        camera.y = event.offsetY - ((event.offsetY - camera.y) / scaleBefore) * camera.scale
-    })
-
-    document.getElementById('canvas').addEventListener('mouseenter', () => {
-        window.requestAnimationFrame(draw)
-        visor.visible = true
-    })
-
-    document.getElementById('canvas').addEventListener('mouseleave', () => {
-        window.requestAnimationFrame(draw)
-        visor.visible = false
-    })
 }
 
-const draw = (ms = 0, dms = 0) => {
+const draw = (canvas, ctx, camera, ms, dms) => {
     const begin = performance.now()
-    const canvas = document.getElementById('canvas')
-    const ctx = canvas.getContext('2d')
-
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     ctx.save()
     ctx.translate(camera.x, camera.y)
     ctx.scale(camera.scale, camera.scale)
     {
-        const view = getView()
+        const view = getView(canvas, camera)
         {
             ctx.save()
 
@@ -101,10 +56,10 @@ const draw = (ms = 0, dms = 0) => {
             const axv = evaluate(ax, time); const ayv = evaluate(ay, time)
             const jxv = evaluate(jx, time); const jyv = evaluate(jy, time)
 
-            if (document.querySelector('input#sdi').checked) new Vector([rxv, ryv]).draw(ctx, 10, 'red')
-            if (document.querySelector('input#svi').checked) new Vector([rxv, ryv], [vxv, vyv]).draw(ctx, 10, 'green', 1/5)
-            if (document.querySelector('input#sai').checked) new Vector([rxv, ryv], [axv, ayv]).draw(ctx, 10, 'blue', 1/5)
-            if (document.querySelector('input#sji').checked) new Vector([rxv, ryv], [jxv, jyv]).draw(ctx, 10, 'purple', 1/15)
+            if (document.querySelector('input#sdi').checked) new Vector([rxv, ryv]).draw(ctx, camera, 8, 'red')
+            if (document.querySelector('input#svi').checked) new Vector([rxv, ryv], [vxv, vyv]).draw(ctx, camera, 8, 'green', 1/5)
+            if (document.querySelector('input#sai').checked) new Vector([rxv, ryv], [axv, ayv]).draw(ctx, camera, 8, 'blue', 1/5)
+            if (document.querySelector('input#sji').checked) new Vector([rxv, ryv], [jxv, jyv]).draw(ctx, camera, 8, 'purple', 1/15)
             ctx.beginPath()
             ctx.arc(rxv, -ryv, 3 / camera.scale, 0, Math.PI * 2)
             ctx.fillStyle = 'black'
@@ -124,8 +79,7 @@ const draw = (ms = 0, dms = 0) => {
     if (dms) ctx.fillText(`${dms.toFixed(3)} ms`, 10, 40)
 }
 
-const getView = () => {
-    const canvas = document.getElementById('canvas')
+const getView = (canvas, camera) => {
     return {
         x1: -camera.x / camera.scale,
         y1: -camera.y / camera.scale,
@@ -172,5 +126,5 @@ const parseExpr = e => {
         jy = completeSimplify(derivativeExpr(ay, 't'))
     }
 
-    draw()
+    // draw()
 }
